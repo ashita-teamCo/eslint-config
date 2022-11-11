2eadc5570762c4444fa0eda4dcc36097e6af529e
mojahige <mojahige@gmail.com>
2022-11-11 15:14:14 +0900

    add import config
    Co-authored-by: Ya-masou <Ya-masou@users.noreply.github.com>
    Co-authored-by: mitani24 <mitani24@users.noreply.github.com>

---
+++ import.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["plugin:import/recommended", "plugin:import/typescript"],
+  rules: {
+    "import/newline-after-import": "warn",
+    "import/order": "warn",
+  },
+};
