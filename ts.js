36f7487e597f15f18ee88df48b0cb7c5e667779b
mitani24 <h.mitani@ashita-team.com>
2023-09-25 14:39:02 +0900

    TypeScript 関連のルールを修正した
    以下のルール指定は stylistic に既に含まれているため削除
    
    - consistent-indexed-object-style
    
    以下のルール指定は recommended-type-checked に既に含まれているため削除
    
     - no-unsafe-member-access
     - no-unsafe-assignment
     - restrict-template-expressions

--- ts.js
+++ ts.js
@@ -1,16 +1,17 @@
 module.exports = {
-  extends: ["plugin:@typescript-eslint/recommended"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/stylistic",
+  ],
   plugins: ["@typescript-eslint"],
   parser: "@typescript-eslint/parser",
   rules: {
-    // どのルールを採用するかは議論が必要
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       { prefer: "type-imports" },
     ],
     "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-    "@typescript-eslint/consistent-indexed-object-style": ["error", "record"],
     "@typescript-eslint/consistent-type-assertions": [
       "error",
       {
